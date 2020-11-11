b257f3dc340bb55fa188aa24dca4d85cf720c40b
Arindam Deb <Arindamdeb2000@icloud.com>
2020-11-11 20:34:25 +0530

    Update Kali_run.py

--- Kali_run.py
+++ Kali_run.py
@@ -1,2 +1,4 @@
 cd ./Desktop/social_mapper
 python3 social_mapper.py -f imagefolder -i ./Input-Examples/imagefolder -m accurate -fb -tw
+python3 social_mapper.py -f imagefolder -i ./Input-Examples/imagefolder -m accurate -fb -tw -ig -li -s -w
+
